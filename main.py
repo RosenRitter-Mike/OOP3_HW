bb1bd015a5c765e5cb42d1684cd3c57b649aa4c1
RosenRitter-Mike <mike88roze@gmail.com>
2025-01-18 08:03:45 +0200

    OOP3_v1.01

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ from datetime import datetime, timedelta
 from pprint import pprint
 
 cheddar = FoodProduct('Cheddar', 13, 'dairy', '07-12-2024', '15-02-2025')
-tenderloin = FoodProduct('Tenderloin', 50, 'meat', '15-01-2025', '28-02-2025')
-bok_choy = FoodProduct('Bok choy', 6, 'parve', '17-01-2025', '27-01-2025')
-peanut_butter = FoodProduct('JIF peanut butter', 13, 'parve', '17-12-2024', '17-02-2025')
+tenderloin = FoodProduct('Tenderloin', 50, 'meat', '15.01.2025', '28.02.2025')
+bok_choy = FoodProduct('Bok choy', 6, 'parve', '17/01/2025', '27/01/2025')
+peanut_butter = FoodProduct('JIF peanut butter', 13, 'parve', '12-17-2024', '02-17-2025')
+matsoni = FoodProduct('Matsoni caspian sea yogurt ', 10, 'dairy', datetime.now() - timedelta(days=1), datetime.now() + timedelta(days=30))
+char_siu = FoodProduct('Cantonese smoked duck', 40, 'meat', '2025-01-15', '2025-02-28')
 
 print(cheddar)
-# print(f"Remaining days: {cheddar.remaining}")
 
 prog.insert_product(cheddar)
 prog.insert_product(tenderloin)
 prog.insert_product(bok_choy)
 prog.insert_product(peanut_butter)
+prog.insert_product(matsoni)
+prog.insert_product(char_siu)
 
 pprint(prog.get_products())
